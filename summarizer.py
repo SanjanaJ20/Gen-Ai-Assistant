3690888a522aedebc5c86f623b8e358eb58b3fc7
Sanjana Jain <sanjanajain206@gmail.com>
2025-07-05 14:05:07 +0530

    Create summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,10 @@
+from transformers import pipeline
+
+# Load summarizer model
+summarizer = pipeline("summarization", model="sshleifer/distilbart-cnn-12-6")
+
+def summarize_document(text):
+    text = text.strip().replace("\n", " ")
+    text = text[:4000]  # limit input size
+    result = summarizer(text, max_length=150, min_length=30, do_sample=False)
+    return result[0]['summary_text']

a2ea47df831940ad33ecb97970285c52b2b8183a
Sanjana Jain <sanjanajain206@gmail.com>
2025-07-07 17:25:23 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -8,3 +8,4 @@ def summarize_document(text):
     text = text[:4000]  # limit input size
     result = summarizer(text, max_length=150, min_length=30, do_sample=False)
     return result[0]['summary_text']
+
